15fde8ec97d24315863d5ae5743054ab62e2d94e
Nick Wylynko <nick@wylynko.com>
2023-07-25 11:59:03 +0800

    gucci gang

--- src/helloworld.c
+++ src/helloworld.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_INPUT_SIZE 100
 
+int scanInt(char *message)
+{
+  int input = 0;
+
+  printf("%s", message);
+  scanf("%d", &input);
+
+  return input;
+}
+
+char *scanStr(char *message)
+{
+  char *input = (char *)malloc(MAX_INPUT_SIZE * sizeof(char));
+
+  if (input == NULL)
+  {
+    printf("Failed to allocate memory.\n");
+    exit(1);
+  }
+
+  printf("%s", message);
+  scanf("%99s", input);
+
+  return input;
+}
+
 int main(void)
 {
-  char input1[MAX_INPUT_SIZE];
-  int input2 = 0;
-
-  printf("Enter a string: ");
-  scanf("%s", input1);
-
-  printf("Enter a number: ");
-  scanf("%d", &input2);
+  char *input1 = scanStr("Enter a string: ");
+  int input2 = scanInt("Enter a number: ");
 
   /* displays output */
   printf("[input1] 's' You entered: %s\n", input1);
-  printf("[input1] 'd' You entered: %d\n", input1);
   printf("[input1] 'p' Address of input1: %p\n", (void *)input1);
 
   printf("[input2] 'd' You entered: %d\n", input2);
+  printf("[input2] 'p' You entered: %p\n", (void *)input2);
+
+  free(input1); /* remember to free the dynamically allocated memory */
 
   return 0;
 }
