bcadf2b3a8b60c62e620bac4384bf0dccf654d5c
Nick Wylynko <nick@wylynko.com>
2023-07-25 11:15:29 +0800

    shits wild

--- src/helloworld.c
+++ src/helloworld.c
@@ -4,15 +4,21 @@
 
 int main(void)
 {
-  char input[MAX_INPUT_SIZE];
+  char input1[MAX_INPUT_SIZE];
+  int input2 = 0;
 
-  printf("Enter an integer: ");
+  printf("Enter a string: ");
+  scanf("%s", input1);
 
-  /* reads and stores input */
-  scanf("%s", input);
+  printf("Enter a number: ");
+  scanf("%d", &input2);
 
   /* displays output */
-  printf("You entered: %s", input);
+  printf("[input1] 's' You entered: %s\n", input1);
+  printf("[input1] 'd' You entered: %d\n", input1);
+  printf("[input1] 'p' Address of input1: %p\n", (void *)input1);
+
+  printf("[input2] 'd' You entered: %d\n", input2);
 
   return 0;
 }
